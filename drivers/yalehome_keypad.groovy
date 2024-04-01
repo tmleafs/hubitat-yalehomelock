05ee9399b2f16aa3d853f1af680718a962dda670
Steven Dale <dale.steven@gmail.com>
2024-04-01 14:08:13 +0100

    Update and rename august_keypad.groovy to yalehome_keypad.groovy

--- drivers/yalehome_keypad.groovy
+++ drivers/yalehome_keypad.groovy
@@ -1,5 +1,5 @@
 /*
-Hubitat Driver For August Door Lock
+Hubitat Driver For Yale Home Keypad
 Copyright 2020 - Taylor Brown
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -8,14 +8,15 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-12-8-2020 :  Initial 
+12-08-2020 :  Initial
+01-04-2024: Yale Home Rebrand 
 */
 
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 
 metadata {
-    definition (name: "August Keypad", namespace: "thecloudtaylor", author: "Taylor Brown") {
+    definition (name: "Yale Home Keypad", namespace: "tmleafs", author: "Taylor Brown") {
         capability "Refresh"
         capability "LockCodes"
         attribute "Battery Level", "string"
