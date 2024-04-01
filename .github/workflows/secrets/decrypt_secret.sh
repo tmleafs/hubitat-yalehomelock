468fcf671a69977f0f54f2600096fb4d65feae52
Steven Dale <dale.steven@gmail.com>
2024-04-01 14:24:58 +0100

    Delete .github/workflows directory

--- .github/workflows/secrets/decrypt_secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-mkdir $HOME/secrets
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPTION_KEY" \
---output $HOME/secrets/id_rsa ./.github/workflows/secrets/package_repo.enc
